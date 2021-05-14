6030e252f51343e27f22d3c8d358761160229acd
SEUNGRO LEE <ysr1227@naver.com>
2021-05-14 18:14:30 +0900

    테스트

--- temp/src/temp/TestFile.java
+++ temp/src/temp/TestFile.java
@@ -1,24 +1,7 @@
 package temp;
 
 public class TestFile {
-
-
-
-
-
-		
-		
-
-	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("하잉ㅎ");
-=======
-		System.out.println("하잉ㅎ");
-
->>>>>>> branch 'main' of https://github.com/hiphop5782/kh7semi.git
-		System.out.println("후우 몇번을 지웠다했는지..");
-		System.out.println("하이");
+// 수정
 	}
 
- }
 
