bf5a593682a66e62a48a88faeb415b51678676e2
jun44 <jun44@DESKTOP-RE79CRN>
2021-05-14 18:14:31 +0900

    dasdasdsa

--- temp/src/temp/TestFile.java
+++ temp/src/temp/TestFile.java
@@ -7,14 +7,5 @@ public class TestFile {
 
 
 		
-		
-
-	public static void main(String[] args) {
-		System.out.println("하잉ㅎ");
-
-		System.out.println("후우 몇번을 지웠다했는지..");
-		System.out.println("하이");
-	}
-
  }
 
