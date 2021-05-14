e4bbb9d059872acd641ee8ec87962be17e1fa704
swjk78 <swjk78@gmail.com>
2021-05-14 18:06:33 +0900

    김진규 두번째

--- temp/src/temp/TestFile2.java
+++ temp/src/temp/TestFile2.java
@@ -1,17 +1,12 @@
 package temp;
 
 public class TestFile2 {
-<<<<<<< HEAD
 	// 자바맨
-=======
 	public static void main(String[] args) {
 		//아무 내용이나 작성(강사)
 		System.out.println("테스트");
-<<<<<<< HEAD
 		// 자바맨
-=======
 		System.out.println("변경 코드");
->>>>>>> branch 'main' of https://github.com/hiphop5782/kh7semi.git
+		System.out.println("???");
 	}
->>>>>>> branch 'main' of https://github.com/hiphop5782/kh7semi.git
 }
