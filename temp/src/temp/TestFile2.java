13145748482421af61c9104c3b92cf4aff2cc87f
82105 <82105@121.172.103.89>
2021-05-14 18:14:22 +0900

    내용삭제

--- temp/src/temp/TestFile2.java
+++ temp/src/temp/TestFile2.java
@@ -2,12 +2,7 @@ package temp;
 
 public class TestFile2 {
 	public static void main(String[] args) {
-		//아무 내용이나 작성(강사)
-		System.out.println("테스트");
-		// 자바맨zz
-		System.out.println("변경 코드");
-		//해결
-		//변경하기
+		
 	}
 
 }
