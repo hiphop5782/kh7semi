60c3361b69c14a9612cd991e8e7624bf01762d99
82104 <82104@192.168.123.155>
2021-05-14 18:20:16 +0900

    # WARNING: head commit changed in the meantime
    빠르네요

--- temp/src/temp/TestFile.java
+++ temp/src/temp/TestFile.java
@@ -10,15 +10,7 @@ public class TestFile {
 		
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("하잉ㅎ");
-=======
-		System.out.println("하잉ㅎ");
 
->>>>>>> branch 'main' of https://github.com/hiphop5782/kh7semi.git
-		System.out.println("후우 몇번을 지웠다했는지..");
-		System.out.println("하이");
-	}
-
+		System.out.println("시스아웃"); 
  }
-
+}
