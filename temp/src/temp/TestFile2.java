1aa31769e1edf2f67feb05fc3b34578e03ad4cd4
user1 <user1@DESKTOP-AC3QINP>
2021-05-14 18:14:22 +0900

    내용삭제

--- temp/src/temp/TestFile2.java
+++ temp/src/temp/TestFile2.java
@@ -2,11 +2,6 @@ package temp;
 
 public class TestFile2 {
 	public static void main(String[] args) {
-		//아무 내용이나 작성(강사)
-		System.out.println("테스트");
-		// 자바맨zz
-		System.out.println("변경 코드");
-		//해결
 	}
 
 }
--- temp/src/temp/TestFileGo.java
+++ temp/src/temp/TestFileGo.java
@@ -2,7 +2,5 @@ package temp;
 
 public class TestFileGo {
 	public static void main(String[] args) {
-		System.out.println("가즈아~");
-		//안녕하세요~
 	}
 }
