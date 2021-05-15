6ebaac15684e0da269481e16e84cf21cad6f4486
User <User@222.112.232.223>
2021-05-15 15:18:56 +0900

    branch

--- temp/src/temp/TestFile2.java
+++ temp/src/temp/TestFile2.java
@@ -4,11 +4,8 @@ public class TestFile2 {
 	public static void main(String[] args) {
 		//아무 내용이나 작성(강사)
 		System.out.println("테스트");
-		// 자바맨zz
 		System.out.println("변경 코드");
-		//해결
-		//변경하기
-		//더워요
+		System.out.print("연습");
 	}
 
 }
