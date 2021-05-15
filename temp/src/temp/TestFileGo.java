ed05c1464b3facc79426909f8ea5fc6e051ced1a
User <User@222.112.232.223>
2021-05-15 15:20:12 +0900

    branch

--- temp/src/temp/TestFileGo.java
+++ temp/src/temp/TestFileGo.java
@@ -3,13 +3,7 @@ package temp;
 public class TestFileGo {
 	public static void main(String[] args) {
 		System.out.println("가즈아~");
-
-		
-		//마하하바바바바바바ㅣ
-
-		//안녕하세요~
-		
-		//되었는가
+		System.out.print("연습");
 
 	}
 }
