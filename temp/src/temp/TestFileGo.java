a622264dc1c6b9f4ef6e9d43acb16ab6dabe1818
user1 <user1@DESKTOP-AC3QINP>
2021-05-18 14:47:22 +0900

    5월 18일

--- temp/src/temp/TestFileGo.java
+++ temp/src/temp/TestFileGo.java
@@ -4,6 +4,6 @@ public class TestFileGo {
 	public static void main(String[] args) {
 		System.out.println("가즈아~");
 		System.out.println("테스트 연동 05월 15일 11시 16분");
-
+		System.out.println("5월 18일");
 	}
 }
